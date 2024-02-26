f02e6769b0627e49b432581d582eabc450ca0bbf
Anusha Kalyani Muttu <53428898+AnushaKalyaniMuttu@users.noreply.github.com>
2024-02-26 23:45:38 +0530

    Add files via upload

--- app.module.ts
+++ app.module.ts
@@ -1,22 +1,23 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
-import {AppComponent} from './app.component';
-import {ServerComponent} from './server/server.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 @NgModule({
-  declarations : [
-  AppComponent,
-  ServerComponent
-    ],
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+  ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
-    ],
+    AppRoutingModule,
+    FormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
-}]
-export class AppModule {
-}
-  
+})
+export class AppModule { }
